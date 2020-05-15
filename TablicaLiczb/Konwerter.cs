6f728eb100bb61787535834a3b394be7cecc565c
Adrian Głąbik <46246127+edenlandpl@users.noreply.github.com>
2020-05-15 15:19:31 +0200

    GotowaWersja

---
+++ TablicaLiczb/Konwerter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Konsola2
+{
+    public class Konwerter
+    {
+        public List<int> parzyste = new List<int>();
+        public List<int> nieparzyste = new List<int>();
+        private List<int> testowaneCyfry = new List<int>();
+        
+        public void SegregujParzysteNieparzyste(List<int> doSortowaniaCyfry)
+        {
+            foreach (var cyfra in doSortowaniaCyfry)
+            {
+                if (cyfra % 2 != 0)
+                    nieparzyste.Add(cyfra);
+                else
+                    parzyste.Add(cyfra);
+            }
+        }
+
+        public void WyswietlCyfry(string komunikat, List<int> cyfry)
+        {
+            Console.Write(komunikat + " - ");
+            foreach (int cyfra in cyfry)
+            {
+                Console.Write(cyfra + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public void resetujListy()
+        {
+            nieparzyste.Clear();
+            parzyste.Clear();
+            testowaneCyfry.Clear();
+        }
+
+        public bool TestujPoprawność(string pobranyCiagBezSpacji)
+        {
+            string tylkoCyfry = "";
+            bool poprawnosc = false;
+
+            foreach (char znak in pobranyCiagBezSpacji)
+            {
+                if (char.IsDigit(znak))
+                {
+                    tylkoCyfry += znak;
+                }
+                if (znak == ',' || !char.IsDigit(znak) || znak == '\0')
+                {
+                    try
+                    {
+                        testowaneCyfry.Add(Int32.Parse(tylkoCyfry));
+                        tylkoCyfry = "";
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Błąd - wpisana litera zamiast liczby lub nie wprowadzono liczby ");
+                        poprawnosc = true;
+                    }
+                }
+                if (znak == '.')
+                {
+                    Console.WriteLine("Tylko liczby calkowite, wpisales liczbe z czescia ulamkowa");
+                    poprawnosc = true;
+                }
+            }
+            return poprawnosc;
+        }
+    }
+}
--- TablicaLiczb/Program.cs
+++ TablicaLiczb/Program.cs
@@ -1,12 +1,78 @@
-﻿git addusing System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace TablicaLiczb
+namespace Konsola2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool programAktywny = true;
+            Konwerter konwerter = new Konwerter();
+            List<int> wpisaneCyfry = new List<int>();
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Wciśnij y, aby wpisać liczby");
+                Console.WriteLine("Wciśnij n, abu zakończyć");
+                char menu = Console.ReadKey().KeyChar;
+
+                switch (menu)
+                {
+                    case 'n':
+                        {
+                            Console.WriteLine("Wybrałeś n");
+                            programAktywny = false;
+                        }
+                        break;
+
+                    case 'y':
+                        {
+                            Console.Clear();
+                            konwerter.resetujListy();
+                            Console.WriteLine("Wybrałeś y");
+                            Console.WriteLine("Wpisz liczby calkowite po przecinku, zatwierdz Enter");
+                            // testowy ciąg liczb
+                            //string pobranyCiag = "1,2,1  1 ,9,77";   
+                            string pobranyCiag = Console.ReadLine();
+
+                            var pobranyCiagBezSpacji = pobranyCiag.Replace(" ", String.Empty);
+
+                            if (konwerter.TestujPoprawność(pobranyCiagBezSpacji))
+                            {
+                                Console.WriteLine("Wpisałeś - " + pobranyCiagBezSpacji);
+                                break;
+                            }
+                            
+                            var numbers = Regex.Split(pobranyCiagBezSpacji, @"[^(0-9)]+").ToList();
+                            numbers = numbers.Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+
+                            try
+                            {
+                                wpisaneCyfry = numbers.Select(s => int.Parse(s)).ToList();
+                            }
+                            catch (Exception e)
+                            {
+
+                                Console.WriteLine("Liczba wykracza poza zakres, wpisz mniejsza"); 
+                                break;
+                            }
+
+                            konwerter.SegregujParzysteNieparzyste(wpisaneCyfry);
+                            konwerter.WyswietlCyfry("Nieparzyste", konwerter.nieparzyste);
+                            konwerter.WyswietlCyfry("Parzyste   ", konwerter.parzyste);
+                            Console.WriteLine("Suma - " + wpisaneCyfry.Sum());
+                        }
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Nie rozpoznano znaku, wcisnij y lub n");
+                        break;
+                }
+            } while (programAktywny);
         }
     }
 }
